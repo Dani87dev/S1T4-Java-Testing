58783c229f7d4a92cfc0bb462ffc6204176c2887
Daniel Vilà <danivila.dev@gmail.com>
2026-01-18 21:33:08 +0100

    feat: implement exception exercice 3 level 1

---
+++ src/main/java/n1exercice3/ExceptionClass.java
@@ -0,0 +1,12 @@
+package n1exercice3;
+
+public class ExceptionClass {
+
+
+    public void makingException() {
+        int[] nums = {1, 2, 3};
+        int value = nums[5]; // Exception --> ArrayIndexOutOfBoundsException
+    }
+
+
+}
---
+++ src/test/java/n1exercice3/ExceptionClassTest.java
@@ -0,0 +1,19 @@
+package n1exercice3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ExceptionClassTest {
+
+    @Test
+    void shouldThrowArrayIndexOutOfBoundsException() {
+        ExceptionClass obj = new ExceptionClass();
+
+        assertThrows(
+                ArrayIndexOutOfBoundsException.class,
+                () -> obj.makingException()
+        );
+    }
+
+}
