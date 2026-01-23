18ca11b4b27635fbad82e8abe423bfe8bfd68a87
Daniel Vilà <danivila.dev@gmail.com>
2026-01-23 14:01:23 +0100

    refactor: simplify Book naming

--- src/main/java/n1exercice1/Book.java
+++ src/main/java/n1exercice1/Book.java
@@ -2,27 +2,27 @@ package n1exercice1;
 
 public class Book implements Comparable<Book> {
 
-    private String titleBook;
+    private String title;
 
     public Book(String titleBook) {
-        this.titleBook = titleBook;
+        this.title = titleBook;
     }
 
     public String getTitleBook() {
-        return titleBook;
+        return this.title;
     }
 
     public void setTitleBook(String titleBook) {
-        this.titleBook = titleBook;
+        this.title= titleBook;
     }
 
     @Override
     public String toString() {
-        return this.titleBook;
+        return this.title;
     }
 
     @Override
     public int compareTo(Book other) {
-        return this.titleBook.compareToIgnoreCase(other.titleBook);
+        return this.title.compareToIgnoreCase(other.title);
     }
 }
